1938a33c61198cf042913d9c4e01d4107b7db04f
Ayush,Mishra <am7876388@gmail.com>
2024-12-12 12:07:06 +0530

    Commited

--- leetcode.js
+++ leetcode.js
@@ -1,4 +1,34 @@
-function square(number){
-return number * number;
+// function fibonacci(n){
+//     if(n === 1){
+//         return 0;
+//     }
+//     else if(n === 2){
+//         return 1;
+//     }
+//     else{
+//     let num1 = fibonacci(n - 1) + fibonacci(n - 2);
+//     return num1;
+//     }
+// }
+// console.log(fibonacci(10));
+class Ayush {
+    constructor(name,Class){
+        this.username = name;
+        this.Class = Class;
+        this.ClassDetails = this.ClassDetails.bind(this);
+    }
 }
-console.log(square(square(2)) + square(3)); 
+Ayush.prototype.ClassDetails = () => {
+    console.log(`${this.Class} Present`);
+}
+class Divya extends Ayush{
+    constructor(name,Class){
+        super(name,Class);
+        this.print = () =>{
+            console.log(`${this.username} Hello`);
+        }
+    }
+}
+let num1 = new Divya("Ayush","Btech final");
+num1.print();
+num1.ClassDetails();
